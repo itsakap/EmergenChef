5446450f7dfdf83fc0ad9055cac049f71c2a4e88
Adam Kaplan <AdamHKaplan@gmail.com>
2014-02-12 14:47:58 -0800

    add email functionality to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,4 +84,13 @@ DONT FORGET TO ADD CONFIGURATION FOR EMAIL!!!!!!!!!!!!!!
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+     config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: 'dummyemail90210',
+    password: 'bubblegum369',
+    authentication: 'plain',
+    enable_starttls_auto: true }
 end
