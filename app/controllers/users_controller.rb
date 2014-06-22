f35e4e69183bc99d808e66e375c0699787507bb7
Adam Kaplan <AdamHKaplan@gmail.com>
2014-06-21 19:09:08 -0700

    propagate use of is_admin? helper

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -71,12 +71,12 @@ private
     end
   end
   def admin_required
-    unless "#{current_user.id}" == "#{ENV['EMERGENCHEF_ADMIN_USER_ID']}"
+    unless is_admin?
       redirect_to root_path
     end
   end
   def user_match_required
-    unless ("#{current_user.id}" == "#{params[:id]}") || ("#{current_user.id}" == "#{ENV['EMERGENCHEF_ADMIN_USER_ID']}")
+    unless ("#{current_user.id}" == "#{params[:id]}") || (is_admin?)
       redirect_to root_path
     end
   end
